f453dd05d0fc8062ec9be74a40afe39b481d0b7b
maor94 <91365490+maor94@users.noreply.github.com>
2022-09-27 04:24:42 -0500

    Missing how to determine winner

--- developer/Unit01/Program.cs
+++ developer/Unit01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Unit01
 {
@@ -6,7 +7,120 @@ namespace Unit01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GreetingDisplay();
+
+            List<string> board = GetNewBoard();
+            
+
+            BoardDisplay(board);
+
+            string currentPlayer = "X";
+
+            int squareChoice = GetUserChoice(currentPlayer);
+            MakeMove(board, currentPlayer, squareChoice);
+            BoardDisplay(board);
+            Console.WriteLine(" "); //Empty Line
+            
+            currentPlayer = "O";
+
+            squareChoice = GetUserChoice(currentPlayer);
+            MakeMove(board, currentPlayer, squareChoice);
+            BoardDisplay(board);
+            Console.WriteLine(" "); //Empty Line
+
+            currentPlayer = "X";
+
+            squareChoice = GetUserChoice(currentPlayer);
+            MakeMove(board, currentPlayer, squareChoice);
+            BoardDisplay(board);
+            Console.WriteLine(" "); //Empty Line
+            
+            currentPlayer = "O";
+
+            squareChoice = GetUserChoice(currentPlayer);
+            MakeMove(board, currentPlayer, squareChoice);
+            BoardDisplay(board);
+            Console.WriteLine(" "); //Empty Line
+
+            currentPlayer = "X";
+
+            squareChoice = GetUserChoice(currentPlayer);
+            MakeMove(board, currentPlayer, squareChoice);
+            BoardDisplay(board);
+            Console.WriteLine(" "); //Empty Line
+            
+            currentPlayer = "O";
+
+            squareChoice = GetUserChoice(currentPlayer);
+            MakeMove(board, currentPlayer, squareChoice);
+            BoardDisplay(board);
+            Console.WriteLine(" "); //Empty Line
+
+            currentPlayer = "X";
+
+            squareChoice = GetUserChoice(currentPlayer);
+            MakeMove(board, currentPlayer, squareChoice);
+            BoardDisplay(board);
+            Console.WriteLine(" "); //Empty Line
+            
+            currentPlayer = "O";
+
+            squareChoice = GetUserChoice(currentPlayer);
+            MakeMove(board, currentPlayer, squareChoice);
+            BoardDisplay(board);
+            Console.WriteLine(" "); //Empty Line
+
+           currentPlayer = "X";
+
+            squareChoice = GetUserChoice(currentPlayer);
+            MakeMove(board, currentPlayer, squareChoice);
+            BoardDisplay(board);
+            Console.WriteLine(" "); //Empty Line
+
+            Console.WriteLine("Thanks for Playing");
+
+        }
+///////////////////
+        static int GetUserChoice(string currentPlayer)
+        {
+            Console.Write($"{currentPlayer}'s turn to choose a square (1-9): ");
+            int squareChoice = int.Parse(Console.ReadLine()); //Changing the user's input from string to integer
+            return squareChoice;
+        }
+
+///////////////////
+        static void MakeMove(List<string>board, string currentPlayer, int squareChoice)
+        {
+            int boardIndex = squareChoice - 1;
+            board[boardIndex] = currentPlayer;
+        }
+
+///////////////////
+        static List<string> GetNewBoard()
+        {   
+            List<string> board = new List<string>();
+            for(int i = 1; i<10; i++)
+            {
+                board.Add(i.ToString());
+            }
+
+            return board;
+        }
+
+//////////////////
+        static void BoardDisplay(List<string> board)
+        {
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+        }
+
+//////////////////
+        static void GreetingDisplay()
+        {
+            Console.WriteLine("Welcome to the Tic Tac Toe Program!");
         }
     }
 }
